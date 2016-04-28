b52656fd1a64d576bb6c834419aae99db64dcb93
Stas Karpov <karpov@kodeks.ru>
2016-04-28 14:21:44 +0300

    more comments and switching to zsh instantly

--- customize.sh
+++ customize.sh
@@ -1,26 +1,39 @@
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Install oh my zsh"
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" > /dev/null
+    echo "To do this please go to https://github.com/robbyrussell/oh-my-zsh"
+    exit
 fi
 
-echo "Oh my zsh - success"
 if [ -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
-    echo "Powerlevel update"
+    echo "Powerlevel9k is installed - updating"
     git -C ~/.oh-my-zsh/custom/themes/powerlevel9k pull
 else
-    echo "No powerlevel 9k theme is installed"
+    echo "Powerlevel9k theme is not installed - installing"
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 fi
-echo "Powerlevel9k - success"
 
 DOTFILES_DIR="$HOME/df"
 if [ ! -d "$DOTFILES_DIR" ]; then
+    echo "Installing dotfiles"
     git clone https://github.com/staskjs/dotfiles.git $DOTFILES_DIR
 else
+    echo "Updating dotfiles"
     git -C $DOTFILES_DIR pull
 fi
 
+echo "Installing .zshrc"
 rm ~/.zshrc
 cp $DOTFILES_DIR/zshrc ~/.zshrc
 
-zsh
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    # If this platform provides a "chsh" command (not Cygwin), do it, man!
+    if hash chsh >/dev/null 2>&1; then
+      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    # Else, suggest the user do so manually.
+    else
+      printf "I can't change your shell automatically because this system does not have chsh.\n"
+      printf "Please manually change your default shell to zsh!\n"
+    fi
+fi
+env zsh
