2bc1d30ec0db731f7e34033bc4d07c5e1f400e12
Stas Karpov <staskjs@mail.ru>
2023-04-27 14:16:48 +0600

    Install nvim

--- install.sh
+++ install.sh
@@ -127,6 +127,10 @@ if [ "$DOTFILES_VIM" -eq 1 ]; then
     ln -s ~/.vim/vimrc ~/.vimrc
 fi
 
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/bin/nvim
+
 echo "Installing nvim config"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 mkdir -p ~/.config
